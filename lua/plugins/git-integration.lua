c65c3e074d4ad6c9959bc996e4f40120f32536cf
ChrisNeugebauer <ChrisNeugebauer@users.noreply.github.com>
2024-02-26 23:29:21 +0100

    Experimenting with lazy loading plugins
    Experimented with lazy loading plugins, it's harder than expected. No change yet

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -1,20 +1,20 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup({
-                on_attach = function(bufnr)
-                    local gs = package.loaded.gitsigns
-                    vim.keymap.set("n", "<leader>gs", gs.stage_buffer, { buffer = bufnr })
-                    vim.keymap.set("n", "<leader>grb", gs.reset_buffer, { buffer = bufnr })
-                    vim.keymap.set("n", "<leader>gb", gs.toggle_current_line_blame, { buffer = bufnr })
-                    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-                end,
-            })
-        end,
-    },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+					vim.keymap.set("n", "<leader>gs", gs.stage_buffer, { buffer = bufnr })
+					vim.keymap.set("n", "<leader>grb", gs.reset_buffer, { buffer = bufnr })
+					vim.keymap.set("n", "<leader>gb", gs.toggle_current_line_blame, { buffer = bufnr })
+					vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+				end,
+			})
+		end,
+	},
 
-    {
-        "tpope/vim-fugitive",
-    },
+	{
+		"tpope/vim-fugitive",
+	},
 }
